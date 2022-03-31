7db4c7c77543fe51d53439e18dfd1d7467ed7b34
Isaac7app <ik.appiagyei@gmail.com>
2022-03-31 20:10:57 +0200

    added code to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import Popular from "./components/Popular";
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import { BrowserRouter } from "react-router-dom";
+import Search from "./components/Search";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
+import { useState, createContext } from "react";
 
 function App() {
+  const UserContext = createContext();
   return (
     <div>
-      <Popular />
+      <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>YumYum</Logo>
+        </Nav>
+        <Search />
+        <Category />
+        <UserContext.Provider value={user}>
+          <Pages />
+        </UserContext.Provider>
+      </BrowserRouter>
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
