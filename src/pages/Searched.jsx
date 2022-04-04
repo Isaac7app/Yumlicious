eafd62229788eb65a69b6b3d44bdddc99d7300ca
Isaac7app <ik.appiagyei@gmail.com>
2022-04-04 22:54:28 +0200

    added styled components to it

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -33,4 +33,25 @@ function Searched() {
   )
 }
 
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  a {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
+
 export default Searched
